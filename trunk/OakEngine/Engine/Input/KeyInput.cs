12bbb3f96292eaa77177002f4273049a294e49b2
LastChicken <LastChicken@083f587b-f4ff-4b6b-aeb3-7530dcb08118>
2010-10-15 14:36:44 +0000

    Added some basic ways to get input. And left a skeleton for some other possible input related things.
    git-svn-id: https://subversion.assembla.com/svn/oak@18 083f587b-f4ff-4b6b-aeb3-7530dcb08118

---
+++ trunk/OakEngine/Engine/Input/KeyInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Oak.Engine.Input
+{
+    class KeyInput
+    {
+        KeyboardState lastState, currentState;
+
+        Keys[] keysPressed;
+
+        public KeyInput()
+        {
+            lastState = new KeyboardState();
+            currentState = Keyboard.GetState();
+        }
+
+        public void Update()
+        {
+            lastState = currentState;
+            currentState = Keyboard.GetState();
+
+            keysPressed = currentState.GetPressedKeys();
+        }
+
+        public bool KeyHeld(Keys key)
+        {
+            return (lastState.IsKeyDown(key) && currentState.IsKeyDown(key));
+        }
+
+        public bool KeyReleased(Keys key)
+        {
+            return (lastState.IsKeyDown(key) && currentState.IsKeyUp(key));
+        }
+
+        public bool NewKeyPress(Keys key)
+        {
+            return (lastState.IsKeyUp(key) && currentState.IsKeyDown(key));
+        }
+
+    }
+}
