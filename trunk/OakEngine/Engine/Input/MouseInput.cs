8270d6b008b101e8ef92aae6ce9e84590d99fdf3
LastChicken <LastChicken@083f587b-f4ff-4b6b-aeb3-7530dcb08118>
2010-10-18 22:54:10 +0000

    git-svn-id: https://subversion.assembla.com/svn/oak@23 083f587b-f4ff-4b6b-aeb3-7530dcb08118

---
+++ trunk/OakEngine/Engine/Input/MouseInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace Oak.Engine.Input
+{
+
+
+    class MouseInput
+    {
+        MouseState lastState, currentState;
+
+        public MouseInput()
+        {
+            lastState = new MouseState();
+            currentState = Mouse.GetState();
+        }
+
+        public void Update()
+        {
+            lastState = currentState;
+            currentState = Mouse.GetState();
+            
+        }
+    }
+}
