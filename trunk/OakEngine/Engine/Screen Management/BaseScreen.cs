15dec51198fdcf65bc64894748045a917fe090a7
alex.laties <alex.laties@083f587b-f4ff-4b6b-aeb3-7530dcb08118>
2010-12-03 21:24:46 +0000

    Changed GameScreen section of the engine to Screen Management, to make its functions more clear
    git-svn-id: https://subversion.assembla.com/svn/oak@41 083f587b-f4ff-4b6b-aeb3-7530dcb08118

---
+++ trunk/OakEngine/Engine/Screen Management/BaseScreen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Oak.Engine.Input;
+
+namespace Oak.Engine.GameScreen
+{
+    public enum ScreenVisibleState
+    {
+        On,
+        Off,
+        Transparent
+    }
+
+    public class BaseScreen
+    {
+        protected KeyboardManager kbm;
+
+        public ScreenVisibleState State
+        {
+            get;
+            set;
+        }
+
+        public BaseScreen()
+        {
+            kbm = new KeyboardManager();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            kbm.Update(gameTime);
+
+            switch (State)
+            {
+                case ScreenVisibleState.On:
+                    UpdateOn(gameTime);
+                    break;
+                case ScreenVisibleState.Off:
+                    UpdateOff(gameTime);
+                    break;
+                case ScreenVisibleState.Transparent:
+                    UpdateTrans(gameTime);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public virtual void UpdateOn(GameTime gameTime)
+        {
+        }
+
+        public virtual void UpdateOff(GameTime gameTime)
+        {
+        }
+
+        public virtual void UpdateTrans(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ trunk/OakEngine/Engine/Screen Management/GameScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oak.Engine.Entities;
+using Oak.Engine.Graphics;
+
+namespace Oak.Engine.GameScreen
+{
+    class GameScreen : BaseScreen
+    {
+        protected BaseWorld world;
+        public BaseWorld World
+        {
+            get { return world; }
+            set { world = value; }
+        }
+
+        public override void UpdateOn(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GraphicsManager.RenderWorld = true;
+            if (GraphicsManager.World != world)
+            {
+                GraphicsManager.World = world;
+            }
+        }
+
+        public override void UpdateOff(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GraphicsManager.RenderWorld = false;
+            base.UpdateOff(gameTime);
+        }
+    }
+}
