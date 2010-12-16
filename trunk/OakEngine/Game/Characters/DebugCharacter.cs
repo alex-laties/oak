2aa680d6e26c1e8069c408aa5fe3198bcbdf8b7c
alex.laties <alex.laties@083f587b-f4ff-4b6b-aeb3-7530dcb08118>
2010-12-16 22:17:05 +0000

    Added the beginnings of a Debug Character class for testing all functionality (physics, collision detection, event handling, etc)
    git-svn-id: https://subversion.assembla.com/svn/oak@45 083f587b-f4ff-4b6b-aeb3-7530dcb08118

---
+++ trunk/OakEngine/Game/Characters/DebugCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oak.Engine.Entities;
+using Oak.Engine.Graphics;
+
+namespace Oak.Game.Characters
+{
+    class DebugCharacter : BaseCharacter
+    {
+        static string PATH_TO_SPRITES = "./Game/Sprites/Player/";
+
+        public DebugCharacter()
+            : base()
+        {
+            Sprite = new StillSprite(PATH_TO_SPRITES + "Rodel");
+            CharacterType = CharacterType.Object;
+        }
+
+    }
+}
